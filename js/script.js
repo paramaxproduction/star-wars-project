2dbad057b9ec3951202fa84eaaadc791afeda744
paramaxproduction <paramaxproduction@gmail.com>
2015-11-23 23:03:51 -0500

    added Javascript

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	$(".darthButton").click(function(){
+		$(".darth").slideToggle(1500);
+	});
+
+	$(".stormButton").click(function(){
+		$(".storm").slideToggle(1500);
+	});
+
+	$(".fettButton").click(function(){
+		$(".fett").slideToggle(1500);
+	});
+
+	$(".r2Button").click(function(){
+		$(".r2").slideToggle(1500);
+	});
+
+	$(".c3Button").click(function(){
+		$(".c3").slideToggle(1500);
+	});
+
+	$(".bbButton").click(function(){
+		$(".bb").slideToggle(1500);
+	});
+});
